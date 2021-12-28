373ac9678db63fb36083fc0dd338c9fefcb36193
marco tosini <>
2021-12-28 17:46:39 +0100

    add applicative

--- src/Parser/Combinator.hs
+++ src/Parser/Combinator.hs
@@ -1,11 +1,11 @@
 module Parser.Combinator where
 
-import Test.Hspec
+import Data.Char (isAlpha, isDigit)
 
-newtype Parser a = NewParser (String -> (Either String a, String))
+newtype Parser a = Parser (String -> (Either String a, String))
 
 charP :: Char -> Parser String
-charP c = NewParser p
+charP c = Parser p
   where
     p "" = (Left "aspetto una stringa non vuota", "")
     p (x : xs)
@@ -13,7 +13,7 @@ charP c = NewParser p
       | otherwise = (Left "aspetto una carattere uguale a c", "")
 
 instance Semigroup a => Semigroup (Parser a) where
-  NewParser sx <> NewParser dx = NewParser p
+  Parser sx <> Parser dx = Parser p
     where
       p s = case sx s of
         (Right x, s') ->
@@ -24,22 +24,72 @@ instance Semigroup a => Semigroup (Parser a) where
         (Left e, _) ->
           (Left e, s)
 
+instance Functor Parser where
+  fmap f (Parser pa) = Parser p
+    where
+      p s = case pa s of
+        (Right a, s') ->
+          (Right $ f a, s')
+        (Left e, _) ->
+          (Left e, s)
+
+instance Applicative Parser where
+  pure = Parser . (,) . Right
+
+  Parser ps <*> Parser pd = Parser p
+    where
+      p s =
+        case ps s of
+          (Right f, s') ->
+            case pd s' of
+              (Right b, s'') ->
+                (Right $ f b, s'')
+              (Left err, _) ->
+                (Left err, s)
+          (Left err, _) ->
+            (Left err, s)
+
 instance Monoid a => Monoid (Parser a) where
-  mempty = NewParser (\s -> (Right mempty, ""))
+  mempty = Parser (\_ -> (Right mempty, ""))
+
+intP :: Parser Int
+intP = fmap read (whileP isDigit)
+
+whileP :: (Char -> Bool) -> Parser String
+whileP f = Parser p
+  where
+    p s =
+      let (l, r) = span f s
+       in (Right l, r)
 
 stringP :: String -> Parser String
 stringP "" = mempty
 stringP (h : t) = charP h <> stringP t
 
+reference2P :: Parser (String, Int)
+reference2P =
+  let px = (whileP isAlpha)
+      dx = intP
+   in ((,) <$> px) <*> dx
+
+referenceP :: Parser (String, Int)
+referenceP = Parser p
+  where
+    p s =
+      let Parser px = (whileP isAlpha)
+          Parser dx = intP
+       in case px s of
+            (Right a, s') ->
+              case dx s' of
+                (Right b, s'') ->
+                  (Right (a, b), s'')
+                (Left err, _) ->
+                  (Left err, s)
+            (Left err, _) ->
+              (Left err, s)
+
 runParser :: Parser a -> String -> (Either String a, String)
-runParser (NewParser f) = f
+runParser (Parser f) = f
 
 execParser :: Parser a -> String -> Either String a
 execParser p s = (fst . runParser p) s
-
-tests :: IO ()
-tests = hspec $ do
-  describe "Pahrser Combinator" $ do
-    it "should parse" $ runParser (charP 'a') "a" `shouldBe` (Right "a", "")
-    it "should parse" $ runParser (charP 'a') "aaa" `shouldBe` (Right "a", "aa")
-    it "should parse" $ runParser (stringP "abcd") "abcd" `shouldBe` (Right "abcd", "")
